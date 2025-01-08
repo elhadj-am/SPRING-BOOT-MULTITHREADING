ed04564af4dc4f8c25214c9336d3dfb006ac4d36
Elhadj <46904847+elhadj-am@users.noreply.github.com>
2025-01-08 18:51:56 +0100

    Update UserController.java

--- multithreading-completableFuture/src/main/java/com/example/multithreading_completableFuture/controller/UserController.java
+++ multithreading-completableFuture/src/main/java/com/example/multithreading_completableFuture/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,7 +17,11 @@ import java.util.concurrent.CompletableFuture;
 public class UserController {
     private  UserService userService;
 
-    @GetMapping(value = "/users", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(value = "/users", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity saveUser(@RequestParam (value = "files") MultipartFile[] files) throws Exception {
         for (MultipartFile file:files){
             userService.saveUser(file);
